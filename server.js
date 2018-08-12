7ece1ced3c203cfe3b967af8c45c9b1ac7402238
hizir aslan <35542226+hzraslan@users.noreply.github.com>
2018-08-12 12:23:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var app = express();
 app.get("/cars", function (request, response){
     response.render('cars');
 })
-app.get('/.well-known/acme-challenge/TuMBkkkQQhTFJg2lfNs4iIOEZLXso4MzzsmyfLEqqyQ', (req, res, next)=>{
-       res.send("TuMBkkkQQhTFJg2lfNs4iIOEZLXso4MzzsmyfLEqqyQ.5Aw91aFZj7I4yUBhIZ4xruwKHjw7CV7LV0-8L6ng2Cs");
-   });
+
 app.get("/micheal", function (request, response){
      var cats_array = {name: "Michael", age: "2", favorite_food: 'meatball', sleeping_spots: 'on the top of couch' };
      response.render('bilgi', {cats: cats_array});
